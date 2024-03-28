7a4c014fd372c7fea0a1ce0a860417d7ed0c908d
PiotrGolon <piotrek98golon@o2.pl>
2024-03-28 19:57:19 +0100

    final

---
+++ components/ui/info.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { Product } from "@/types";
+import Currency from "./currency";
+import Button from "./button";
+import { ShoppingCart } from "lucide-react";
+import useCart from "@/hooks/use-cart";
+
+interface InfoProps {
+  data: Product;
+}
+
+const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-neutral-600">{data.name}</h1>
+      <div className="mt-3 flex items-end justify-between">
+        <p className="text-2xl text-gray-900">
+          <Currency value={data?.price} />
+        </p>
+      </div>
+      <hr className="my-4" />
+      <div className="flex flex-col gap-y-6">
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-neutral-600">Ilość przepisów: </h3>
+          <div className="text-neutral-600">{data?.size?.value}</div>
+        </div>
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-neutral-600">Color:</h3>
+          <div
+            className="h-6 w-6 rounded-full border border-gray-600 "
+            style={{ backgroundColor: data?.color?.value }}
+          />
+        </div>
+      </div>
+      <div className="mt-10 flex items-center gap-x-3">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
+          Dodaj do koszyka
+          <ShoppingCart />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
